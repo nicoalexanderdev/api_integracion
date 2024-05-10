87b4235b3a00e69e7ab7e51d20d13ad2190a0043
nixtoon <127158636+nixtoon@users.noreply.github.com>
2024-05-10 13:52:25 -0400

    api levantado

--- api/models.py
+++ api/models.py
@@ -3,16 +3,16 @@ from django.db import models
 # Create your models here.
 
 class Marca(models.Model):
-  nom_marca = models.Charfield(max_length=80)
+  nom_marca = models.CharField(max_length=80)
 
   def __str__(self):
     return self.nom_marca
 
 class Producto(models.Model):
-  nombre = models.Charfield(max_length=100)
-  precio = models.Integerfield()
-  descripcion = models.Textfield()
-  marca = models.ForeignKey(Marca, on_delete=models.PROTECTED)
+  nombre = models.CharField(max_length=100)
+  precio = models.IntegerField()
+  descripcion = models.TextField()
+  marca = models.ForeignKey(Marca, on_delete=models.PROTECT)
 
 def __str__(self):
   return self.nombre
