0f6a7c7dce4f2e03120c7fc1a46553daf4831938
nixtoon <127158636+nixtoon@users.noreply.github.com>
2024-05-19 17:55:44 -0400

    views documentacion transbank

--- compra/urls.py
+++ compra/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
   path('transaction/create/', views.transbank_create),
   path('transaction/reverse-or-cancel/<str:tokenws>', views.transbank_reverse_or_cancel),
-  path('commit/<str:tokenws>', views.transbank_commit)
+  path('commit/<str:tokenws>', views.transbank_commit),
+  path('get-status/<str:tokenws>/', views.transbank_get_status),
+  path('transbank-capture/<str:tokenws>', views.transbank_capture)
 ]
--- compra/views.py
+++ compra/views.py
@@ -3,23 +3,25 @@ from rest_framework.decorators import api_view
 from django.http import JsonResponse
 import requests
 import json
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
+
 def headers_request_transbank():
-  headers = { # DEFINICIÓN TIPO DE AUTORIZACIÓN Y AUTENTICACIÓN
-                "Authorization": "Token",
-                # LLAVE QUE DEBE SER MODIFICADA PORQUE ES SOLO DEL AMBIENTE DE INTEGRACIÓN DE TRANSBANK (PRUEBAS)
-                "Tbk-Api-Key-Id": "597055555532",
-                # LLAVE QUE DEBE SER MODIFICADA PORQUE DEL AMBIENTE DE INTEGRACIÓN DE TRANSBANK (PRUEBAS)
-                "Tbk-Api-Key-Secret": "579B532A7440BB0C9079DED94D31EA1615BACEB56610332264630D42D0A36B1C",
-                # DEFINICIÓN DEL TIPO DE INFORMACIÓN ENVIADA
-                "Content-Type": "application/json",
-                # DEFINICIÓN DE RECURSOS COMPARTIDOS ENTRE DISTINTOS SERVIDORES PARA CUALQUIER MÁQUINA
-                "Access-Control-Allow-Origin": "*",
-                'Referrer-Policy': 'origin-when-cross-origin',
-                } 
-  return headers   
+    headers = {  # DEFINICIÓN TIPO DE AUTORIZACIÓN Y AUTENTICACIÓN
+        "Authorization": "Token",
+        # LLAVE QUE DEBE SER MODIFICADA PORQUE ES SOLO DEL AMBIENTE DE INTEGRACIÓN DE TRANSBANK (PRUEBAS)
+        "Tbk-Api-Key-Id": "597055555532",
+        # LLAVE QUE DEBE SER MODIFICADA PORQUE DEL AMBIENTE DE INTEGRACIÓN DE TRANSBANK (PRUEBAS)
+        "Tbk-Api-Key-Secret": "579B532A7440BB0C9079DED94D31EA1615BACEB56610332264630D42D0A36B1C",
+        # DEFINICIÓN DEL TIPO DE INFORMACIÓN ENVIADA
+        "Content-Type": "application/json",
+        # DEFINICIÓN DE RECURSOS COMPARTIDOS ENTRE DISTINTOS SERVIDORES PARA CUALQUIER MÁQUINA
+        "Access-Control-Allow-Origin": "*",
+        'Referrer-Policy': 'origin-when-cross-origin',
+    }
+    return headers
 
 
 @api_view(['POST'])
@@ -47,11 +49,12 @@ def transbank_create(request):
         return JsonResponse({'error': 'Error de decodificación JSON: {}'.format(str(e))}, status=500)
 
 
-api_view(['PUT'])
-def transbank_commit(tokenws):
+@csrf_exempt
+@api_view(['PUT'])
+def transbank_commit(request, tokenws):
     try:
         print('tokenws: ', tokenws)
-        
+
         # FORMACIÓN DE LA URL DE TRANSBANK PARA CONFIRMAR UNA TRANSACCIÓN
         url = f"https://webpay3gint.transbank.cl/rswebpaytransaction/api/webpay/v1.2/transactions/{tokenws}"
 
@@ -60,22 +63,47 @@ def transbank_commit(tokenws):
 
         # INVOCACIÓN POR PUT A API REST QUE CONFIRMA UNA TRANSACCIÓN EN TRANSBANK
         response = requests.put(url, headers=headers)
-        print('response: ', response.json())
+        response_data = response.json()
+        print('response: ', response_data)
 
         # RETORNO DE LA RESPUESTA DE TRANSBANK
-        return response.json()
+        return JsonResponse(response_data, safe=False)
 
+    except json.JSONDecodeError as e:
+        return JsonResponse({'error': 'Error de decodificación JSON: {}'.format(str(e))}, status=500)
     except Exception as e:
         # Manejar cualquier excepción que pueda ocurrir durante la solicitud a la API de Transbank
         print(f"Error al confirmar transacción en Transbank: {e}")
-        return {'error': str(e)}
+        return JsonResponse({'error': str(e)}, status=500)
+
+
+@api_view(['GET'])
+def transbank_get_status(request, tokenws):
+    try:
+        print('tokenws: ', tokenws)
+
+        # FORMACIÓN DE LA URL DE TRANSBANK PARA CONFIRMAR UNA TRANSACCIÓN
+        url = f"https://webpay3gint.transbank.cl/rswebpaytransaction/api/webpay/v1.2/transactions/{tokenws}"
+
+        # CABECERA SOLICITADA POR TRANSBANK
+        headers = headers_request_transbank()
+
+        # INVOCACIÓN POR GET A API REST QUE CONFIRMA UNA TRANSACCIÓN EN TRANSBANK
+        response = requests.get(url, headers=headers)
+        print('response: ', response.json())
+
+        # RETORNO DE LA RESPUESTA DE TRANSBANK
+        return JsonResponse(response.json())
+
+    except json.JSONDecodeError as e:
+        return JsonResponse({'error': 'Error de decodificación JSON: {}'.format(str(e))}, status=500)
 
 
 @api_view(['POST'])
 def transbank_reverse_or_cancel(request, tokenws):
     try:
         print('tokenws: ', tokenws)
-        
+
         # LECTURA DEL PAYLOAD (BODY) CON INFORMACIÓN DE TIPO JSON
         data = json.loads(request.body)
         print('data: ', data)
@@ -94,4 +122,24 @@ def transbank_reverse_or_cancel(request, tokenws):
         return JsonResponse(response.json())
 
     except json.JSONDecodeError as e:
-        return JsonResponse({'error': 'Error de decodificación JSON: {}'.format(str(e))}, status=500) 
+        return JsonResponse({'error': 'Error de decodificación JSON: {}'.format(str(e))}, status=500)
+
+
+@api_view(['PUT'])
+def transbank_capture(request, tokenws):
+    try:
+        url = f"https://webpay3gint.transbank.cl/rswebpaytransaction/api/webpay/v1.2/transactions/{tokenws}/capture"
+
+        headers = headers_request_transbank()
+        response = requests.put(url, headers=headers)
+        response_data = response.json()
+        print('response: ', response_data)  
+
+        return JsonResponse(response_data, safe=False)
+
+    except json.JSONDecodeError as e:
+        return JsonResponse({'error': 'Error de decodificación JSON: {}'.format(str(e))}, status=500)
+    except Exception as e:
+        # Manejar cualquier excepción que pueda ocurrir durante la solicitud a la API de Transbank
+        print(f"Error al confirmar transacción en Transbank: {e}")
+        return JsonResponse({'error': str(e)}, status=500)
